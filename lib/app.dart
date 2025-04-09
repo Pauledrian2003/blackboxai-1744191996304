7f0c55047296f1e93d0cab8855820450a1b27850
Pauledrian2003 <113221780+Pauledrian2003@users.noreply.github.com>
2025-04-09 17:47:26 +0800

    Add lib/app.dart 1744192042870

---
+++ lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:real_estate_app/app_router.dart';
+
+class AppWrapper extends ConsumerWidget {
+  const AppWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    return MaterialApp.router(
+      routerConfig: router,
+      title: 'Real Estate App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
